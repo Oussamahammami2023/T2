031fde537c9f9c91badeb13f8dfa64589014586d
CME2024 <142491184+Oussamahammami2023@users.noreply.github.com>
2024-11-21 11:46:45 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'offline-cache-v3'; // قم بتغيير هذا الرقم في كل تحديث
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll([
+        'offline.html',
+        // أضف باقي الملفات التي تريد تخزينها في الكاش
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // حذف الكاش القديم
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
